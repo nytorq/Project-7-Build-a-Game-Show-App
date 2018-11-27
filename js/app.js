53546eb93b1c0290392f4b9668fe5a1ead026a46
Charlie Prator <charlieprator@gmail.com>
2018-11-26 21:28:03 -0700

    No match and checkWin functionality
    Getting hearts to disappear when no match is found. Created the checkWin
    function. Cleaned up som other errors.

--- js/app.js
+++ js/app.js
@@ -10,22 +10,28 @@ const phrase = ['I am a meat-popsicle.',
                 'Why so serious?',
                 'Witness me!']
 let missed = 0;
+let match = 0;
 const startGame = document.getElementsByClassName('btn__reset');
 const phraseBanner = document.querySelectorAll('ul');
 const keyboardButton = document.querySelectorAll('button');
+const tries = document.getElementsByClassName('tries');
+const overlay = document.getElementById('overlay');
+let parsedLetterNumber = '';
+let parsedShownLetterNumber = '';
 let currentLetter = '';
-let checkLetter = 0;
+let heartsLost = 0;
+let letterFound = '';
 
 // FUNCTIONS
 
 function getRandomPhraseArray(array) {
-  const min = 0;
-  const max = phrase.length;
-  return array[Math.floor(Math.random() * (max - min)) + min];
+  // const min = 0;
+  // const max = phrase.length;
+  // return array[Math.floor(Math.random() * (max - min)) + min];
+  return array[2];
 };
 
 startGame[0].addEventListener('click', () => {
-  const overlay = document.getElementById('overlay');
   overlay.style.display = 'none';
 });
 
@@ -33,21 +39,23 @@ startGame[0].addEventListener('click', () => {
 const chosenPhrase = getRandomPhraseArray(phrase);
 const characterArray = [...chosenPhrase];
 
-function characterInserter() {
+function addPhraseToDisplay() {
   for (let i = 0 ; i < characterArray.length ; i++) {
     const newLi = document.createElement('li');
     const currentCharacter = characterArray[i];
     if (/\W/.test(currentCharacter)) {
       newLi.className = 'non-letter';
     } else {
-      newLi.className = 'letter'
+      newLi.className = 'letter';
     }
     newLi.textContent = currentCharacter;
     phraseBanner[0].appendChild(newLi);
+    const letterArray = document.getElementsByClassName('letter');
+    parsedLetterNumber = letterArray.length;
   }
-};
+}
 
-characterInserter();
+addPhraseToDisplay();
 
 
 for (let i = 0 ; i < keyboardButton.length ; i++) {
@@ -58,17 +66,36 @@ for (let i = 0 ; i < keyboardButton.length ; i++) {
       const phraseLetters = document.getElementsByClassName('letter');
       for (let i = 0 ; i < phraseLetters.length ; i++) {
         const currentPhraseLetter = phraseLetters[i].textContent;
+        const smallCurrentPhraseLetter = currentPhraseLetter.toLowerCase();
         const currentButtonLetter = chosenButton.textContent;
-        if (currentPhraseLetter === currentButtonLetter) {
-          checkLetter += 1;
-        }
+        if (smallCurrentPhraseLetter === currentButtonLetter) {
+          phraseLetters[i].className = 'show';
+          letterFound = currentButtonLetter;
+          const shownLetters = document.getElementsByClassName('show');
+          parsedShownLetterNumber = shownLetters.length
+        } else {
+            letterFound = null
+          }
       };
     };
     checkLetter(pressedButton);
-    if (checkLetter > 0) {
-      console.log('no match');
+    if (letterFound === null) {
+      tries[missed].style.display = 'none';
       missed += 1;
-      console.log(missed);
     }
-  })
+    function checkWin() {
+      if (missed === 5) {
+        overlay.style.display = 'inherit';
+        overlay.className = 'lose';
+        let title = document.getElementsByClassName('title');
+        title[0].innerText = 'You have lost the game.';
+      } else if (parsedLetterNumber === parsedShownLetterNumber) {
+        overlay.style.display = 'inherit';
+        overlay.className = 'win';
+        title[0].innerText = 'You have won the game!';
+      }
+    }
+    checkWin();
+  });
+
 };
